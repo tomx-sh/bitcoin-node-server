c593ffaf57d4d7d3078cb8e1e6ef42c550eb8fcb
Thomas DOUCHE <Thomas.douche@aol.com>
2024-10-19 15:25:41 +0200

    fix double log

--- app/loadenv.go
+++ app/loadenv.go
@@ -10,6 +10,7 @@ func LoadEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println("No .env file found. Using system environment variables.")
+		return
 	}
 	fmt.Println(".env loaded successfully.")
 }
